4191812e81ccf23edc788a7628c6c399e1c23c82
Alexa Botello <abotello20@yahoo.com>
2020-10-19 22:49:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-data.each_with_object({}) do |(key, values), a|
+data.each_with_object({}) do |(key, values), final_array|
   final_array[key] = v + 3
 binding.pry
 end 
