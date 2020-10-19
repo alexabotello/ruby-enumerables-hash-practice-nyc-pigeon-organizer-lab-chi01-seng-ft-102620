21ea1ff1f2ad2815109698f7307e7278344aab25
Alexa Botello <abotello20@yahoo.com>
2020-10-19 22:57:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,11 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-data.each_with_object({}) do |(key, value), final_array|
-  final_array
+  data.each_with_object({}) do |(key, value), final_array|
    value.each do |inner_key, names| 
     names.each do |name|
     end 
-  end 
+   end 
   final_array
-end 
+  end 
 end 
