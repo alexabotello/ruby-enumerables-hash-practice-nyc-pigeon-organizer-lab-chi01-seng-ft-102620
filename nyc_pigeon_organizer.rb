b76cf8e4199c5d16e1ad5df4cac858b9d06b52b4
Alexa Botello <abotello20@yahoo.com>
2020-10-19 23:10:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array|
    value.each do |inner_key, names| 
     names.each do |name|
-    end 
       if !final_array[name]
         final_array[name] = {}
       end 
